40e19c5caaf81b6758ab55dc121448b77c45351d
srujanshenoy <srujanshenoy@gmail.com>
2021-11-12 09:31:53 +0530

    Create convertor.py

---
+++ convertor.py
@@ -0,0 +1,7 @@
+print("W N/D")
+Whole_part = int(input("W: "))
+Numerator_of_input = int(input("N: "))
+denominator_of_input = int(input("D: "))
+
+numerator_of_output = Whole_part*denominator_of_input+Numerator_of_input
+print(f"{Whole_part} {Numerator_of_input}/{denominator_of_input} = {numerator_of_output}/{denominator_of_input}")
