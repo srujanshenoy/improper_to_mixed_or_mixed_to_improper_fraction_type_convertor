3769af4f66c28056d0f6be7a50adc689e5d6d35c
srujanshenoy <srujanshenoy@gmail.com>
2021-11-12 10:50:57 +0530

    Upadate convertor.py to check inputs and loop

--- convertor.py
+++ convertor.py
@@ -1,7 +1,24 @@
-print("W N/D")
-Whole_part = int(input("W: "))
-Numerator_of_input = int(input("N: "))
-denominator_of_input = int(input("D: "))
+while True:
+    print("W N/D")
+    Whole_part = input("W: ")
+    if Whole_part == "End Now":
+        print("OK. Ending now...")
+        break
 
-numerator_of_output = Whole_part*denominator_of_input+Numerator_of_input
-print(f"{Whole_part} {Numerator_of_input}/{denominator_of_input} = {numerator_of_output}/{denominator_of_input}")
+    Numerator_of_input = input("N: ")
+
+    if Numerator_of_input == "End Now":
+        print("OK. Ending now...")
+        break
+    else:
+        Numerator_of_input_int = int(Numerator_of_input)
+    denominator_of_input = input("D: ")
+
+    if denominator_of_input == "End Now":
+        print("OK. Ending now...")
+        break
+    else:
+        denominator_of_input_int = int(denominator_of_input)
+
+    numerator_of_output = Whole_part*denominator_of_input+Numerator_of_input
+    print(f"{Whole_part} {Numerator_of_input_int}/{denominator_of_input_int} = {numerator_of_output}/{denominator_of_input_int}")
