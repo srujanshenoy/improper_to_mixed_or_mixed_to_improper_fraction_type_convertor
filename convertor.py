c58b659e3ee30d2ffae3036ebb9e4c1356efb80d
srujanshenoy <srujanshenoy@gmail.com>
2021-11-15 09:08:31 +0530

    make it more readable - convertor.py

--- convertor.py
+++ convertor.py
@@ -1,21 +1,21 @@
 while True:
-    print("W N/D")
+    print("\n\nW N/D")
     print("if you want to end, type End Now exactly into any feild.")
-    Whole_part = input("W: ")
+    Whole_part = input("\nW: ")
     if Whole_part == "End Now":
         print("OK. Ending now...")
         break
     else:
         Whole_part_int = int(Whole_part)
 
-    Numerator_of_input = input("N: ")
+    Numerator_of_input = input("\nN: ")
 
     if Numerator_of_input == "End Now":
         print("OK. Ending now...")
         break
     else:
         Numerator_of_input_int = int(Numerator_of_input)
-    denominator_of_input = input("D: ")
+    denominator_of_input = input("\nD: ")
 
     if denominator_of_input == "End Now":
         print("OK. Ending now...")
