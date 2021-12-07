02942df27d6634b9860f8c1e0029c13cdbd7c32c
srujanshenoy <srujanshenoy@gmail.com>
2021-12-07 20:13:01 +0530

    fixed draft

--- imroper to mixed convertor.py
+++ imroper to mixed convertor.py
@@ -1,9 +1,10 @@
+# credit to shwethab to fixing my program! (thanks hodeaayee)
 while True:
     print("you have entered improper to mixed mode.")
     numerator_of_input = int(input("input the numerator: "))
 
     print(f"you have entered: {numerator_of_input} / D")
-    denominator_of_input = int(input("input the denominator"))
+    denominator_of_input = int(input("input the denominator: "))
 
     if numerator_of_input < denominator_of_input:
         print("oops! this is not an improper fraction. runing program from start!.")
@@ -14,11 +15,8 @@ while True:
 
     print(f"you have entered: {numerator_of_input} / {denominator_of_input}")
 
-    step_1_whole_of_output = numerator_of_input / denominator_of_input
+    step_1_whole_of_output = numerator_of_input // denominator_of_input
     step_2_whole_of_output_or_numerator_of_output = numerator_of_input % denominator_of_input
-    whole_of_output = step_1_whole_of_output - \
-        step_2_whole_of_output_or_numerator_of_output
-    step_2_whole_of_output_or_numerator_of_output
-
+    whole_of_output = step_1_whole_of_output
     denominator_of_output = denominator_of_input
     print(f"you entered: {numerator_of_input} / {denominator_of_input}\n answer: {whole_of_output} {step_2_whole_of_output_or_numerator_of_output} / {denominator_of_output}")
